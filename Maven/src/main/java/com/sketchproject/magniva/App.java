5db42ace05e12bbe8418b8ba2f65f8f4f21e6675
Angga Ari Wijaya <anggadarkprince@gmail.com>
2017-01-19 08:47:19 +0700

    add maven basic

---
+++ Maven/src/main/java/com/sketchproject/magniva/App.java
@@ -0,0 +1,10 @@
+package com.sketchproject.magniva;
+
+/**
+ * Created by angga on 11/12/16.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
